44a8190d4c42d06604b51ba92104f62ea4f0e7a1
Анна Метелькова <ania.metelkova91>
2023-01-12 15:18:11 +0300

    The last option

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main():
+if __name__ == '__main__':
     run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
@@ -89,7 +89,4 @@ def main():
     print('Ты можешь выбрать один из трёх путей силы:')
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
-    print(start_training(char_name, char_class))
-
-
-main()
+    print(start_training(char_name, char_class)) 
